50e719b953de81c30138e30c9e450482e1a61cba
w-oby <wail.ouladbenyoussef@student.rocva.nl>
2025-12-12 21:22:11 +0100

    Defined method to load jokes

--- api/api.py
+++ api/api.py
@@ -2,3 +2,30 @@ import json
 import random
 
 from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Pull up joke data
+def load_jokes():
+
+    try:
+
+        with open('api/jokes.json', 'r') as file:
+
+            data = json.load(file)
+
+            return data['jokes']
+        
+    except FileNotFoundError:
+
+        print("Could not find specified file.\n")
+
+        return []
+    
+    except (json.JSONDecodeError):
+
+        print("JSON is invalid.\n")
+
+        return []
+
+jokes = load_jokes()
