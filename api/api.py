57e0ec25e52db3d6e6fd9b61e9203f5ba9482723
w-oby <wail.ouladbenyoussef@student.rocva.nl>
2025-12-12 21:23:20 +0100

    Routing, more handling and randomization added

--- api/api.py
+++ api/api.py
@@ -29,3 +29,25 @@ def load_jokes():
         return []
 
 jokes = load_jokes()
+
+@app.route('/api/jokes', methods=['GET'])
+
+# Readying the jokes for use with API
+def get_jokes():
+    
+    return jsonify(jokes)
+
+@app.route('/api/jokes/random', methods=['GET'])
+
+# Pulling a random joke from the list
+def get_random_joke():
+
+    if not jokes:
+        
+        error_msg = { "error_message": "Error. Could not load contents from file." }
+
+        return jsonify(error_msg), 500
+    
+    random_joke = random.choice(jokes)
+
+    return jsonify(random_joke)
