e299bbec8366593640c88ae609559a0dfed42ca1
w-oby <wail.ouladbenyoussef@student.rocva.nl>
2026-01-02 08:10:20 +0100

    Batch file to automate the setup of server + open and present CLI window to user

---
+++ start.bat
@@ -0,0 +1,33 @@
+:: USED SOURCES
+:: https://superuser.com/questions/62525/run-a-batch-file-in-a-completely-hidden-way
+:: https://superuser.com/questions/1585731/run-a-bat-file-silently
+:: https://stackoverflow.com/questions/507347/hide-command-window-of-bat-file-that-executes-another-exe-file
+:: https://stackoverflow.com/questions/33685398/how-not-to-open-a-cmd-window-when-running-a-batch-file
+:: https://www.reddit.com/r/Batch/comments/1d5z701/how_to_run_batch_hiddeninvisible/
+:: https://www.tenforums.com/general-support/208200-can-i-run-batch-file-without-cmd-exe-window-popping-up.html
+:: https://stackoverflow.com/questions/8678441/difference-between-wscript-and-cscript
+:: https://serverfault.com/questions/38318/better-way-to-wait-a-few-seconds-in-a-bat-file
+:: https://www.robvanderwoude.com/wait.php
+
+@echo off
+
+title JokeAPI
+
+echo Starting the JokeAPI server in the background...
+
+:: Create a VBScript file that runs the app.py file while in background and without printing
+echo CreateObject("Wscript.Shell").Run "python api/app.py", 0, False > start_jokeapi_server.vbs
+
+:: Run the newly created file
+cscript //nologo start_jokeapi_server.vbs
+
+del start_jokeapi_server.vbs
+
+:: A small pause to ensure the server has started up fully
+timeout /t 3 /nobreak >nul
+
+echo Starting the JokeAPI client...
+echo.
+
+:: Execute the user.py script which prompts a CLI window for the user to see.
+python user.py
